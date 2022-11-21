09b0507d28c6dc4ff3413c68deb91ed756372dd9
Emil Kjærgaard Eybye <Emil.eybye.24@gmail.com>
2022-11-21 21:40:08 +0100

    Update cv, page styling and text.

--- javascript/script.js
+++ javascript/script.js
@@ -1,87 +1,74 @@
 // Remove no-js class
 
-$('html').removeClass('no-js');
+$("html").removeClass("no-js");
 
 window.scroll({
-    behavior: 'smooth'
+  behavior: "smooth",
 });
 
 function getCookie(cname) {
-    const name = cname + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
 
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") {
+      c = c.substring(1);
     }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
 
-    return "";
+  return "";
 }
 
 function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    const expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-const cookieBanner = document.querySelector('#cookie-banner');
-const hasCookieConsent = getCookie('cookies-consent');
+const cookieBanner = document.querySelector("#cookie-banner");
+const hasCookieConsent = getCookie("cookies-consent");
 
 if (!hasCookieConsent) {
-    cookieBanner.classList.remove('hidden');
+  cookieBanner.classList.remove("hidden");
 }
 
-const consentCta = cookieBanner.querySelector('#consent-cookies');
+const consentCta = cookieBanner.querySelector("#consent-cookies");
 
-consentCta.addEventListener('click', () => {
-    cookieBanner.classList.add('hidden');
-    setCookie('cookies-consent', 1, 365);
+consentCta.addEventListener("click", () => {
+  cookieBanner.classList.add("hidden");
+  setCookie("cookies-consent", 1, 365);
 });
 
 document.getElementById("linkedin").onclick = function () {
-    window.open("https://www.linkedin.com/in/emil-kjærgaard-eybye", "_blank");
+  window.open("https://www.linkedin.com/in/emil-kjærgaard-eybye", "_blank");
 };
 
 document.getElementById("github").onclick = function () {
-    window.open("https://github.com/Emil-Ey", "_blank");
-}; 
- 
+  window.open("https://github.com/Emil-Ey", "_blank");
+};
+
 document.getElementById("pro1").onclick = function () {
-    window.open("https://github.com/Emil-Ey/Emil-Ey.github.io", "_blank");
-};
-
-document.getElementById("pro2").onclick = function () {
-    window.open("https://rotundo.dk", "_blank");
-};
-
-document.getElementById("pro3").onclick = function () {
-    window.open("https://eybye-todo.xyz/", "_blank");
+  window.open("https://github.com/Emil-Ey/Emil-Ey.github.io", "_blank");
 };
 
 document.getElementById("pro4").onclick = function () {
-    window.open("https://github.com/Emil-Ey/ToDoList", "_blank");
-};
-
-document.getElementById("pro5").onclick = function () {
-    window.open("https://travelbuddy.one/", "_blank");
+  window.open("https://github.com/Emil-Ey/ToDoList", "_blank");
 };
 
 document.getElementById("pro6").onclick = function () {
-    window.open("https://github.com/Emil-Ey/TravelBuddy", "_blank");
+  window.open("https://github.com/Emil-Ey/TravelBuddy", "_blank");
 };
 
 document.getElementById("cv").onclick = function () {
-    window.open("https://emil-ey.github.io/CV.pdf", "_blank");
+  window.open("https://emil-ey.github.io/CV.pdf", "_blank");
 };
 
 document.getElementById("cv1").onclick = function () {
-    window.open("https://emil-ey.github.io/CV.pdf", "_blank");
+  window.open("https://emil-ey.github.io/CV.pdf", "_blank");
 };
-
